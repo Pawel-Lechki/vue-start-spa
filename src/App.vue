80ae2a1a467e7c369eb45865431771af47b060fa
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-19 18:20:01 +0200

    add simple fetch data

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
 <template>
     <Navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index"></Navbar>
-    <PageViewer :page="pages[activePage]"></PageViewer>
+    <PageViewer 
+        v-if="pages.length > 0"
+        :page="pages[activePage]"></PageViewer>
 </template>
 
 <script>
     import PageViewer from './components/PageViewer.vue';
     import Navbar from './components/Navbar.vue';
+import { resolveTransitionHooks } from 'vue';
 
     export default{
         components: {
             Navbar,
             PageViewer
         },
+        created() {
+            this.getPages();
+        },
         data() {
             return {
                 activePage: 0,
-                pages: [
-                    {
-                        link: {text: 'Home', url: 'index.html'},
-                        pageTitle: 'Home Page',
-                        content: 'This in the home page content'
-                    },
-                    {
-                        link: {text: 'About', url: 'index.html'},
-                        pageTitle: 'About Page',
-                        content: 'This in the about page content'
-                    },
-                    {
-                        link: {text: 'Contact', url: 'index.html'},
-                        pageTitle: 'Contact Page',
-                        content: 'This in the contact page content'
-                    },
-                ],
+                pages: []
+            }
+        },
+        methods: {
+            async getPages() {
+                let res = await fetch('pages.json');
+                let data = await res.json();
+
+                this.pages = data;
             }
         }
     }
