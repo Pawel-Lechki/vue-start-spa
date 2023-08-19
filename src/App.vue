65f2757493e0631a7b91528b173037d77c245934
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-19 21:47:31 +0200

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,47 @@
 <template>
-    <Navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index"></Navbar>
-    <PageViewer 
+  <Navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></Navbar>
+  <!-- <PageViewer 
         v-if="pages.length > 0"
-        :page="pages[activePage]"></PageViewer>
+        :page="pages[activePage]">
+    </PageViewer> -->
+  <create-page :page-created="pageCreated"> </create-page>
 </template>
 
 <script>
-    import PageViewer from './components/PageViewer.vue';
-    import Navbar from './components/Navbar.vue';
-import { resolveTransitionHooks } from 'vue';
+  import PageViewer from "./components/PageViewer.vue"
+  import Navbar from "./components/Navbar.vue"
+  import { resolveTransitionHooks } from "vue"
+  import CreatePage from "./components/CreatePage.vue"
 
-    export default{
-        components: {
-            Navbar,
-            PageViewer
-        },
-        created() {
-            this.getPages();
-        },
-        data() {
-            return {
-                activePage: 0,
-                pages: []
-            }
-        },
-        methods: {
-            async getPages() {
-                let res = await fetch('pages.json');
-                let data = await res.json();
+  export default {
+    components: {
+      Navbar,
+      PageViewer,
+      CreatePage,
+    },
+    created() {
+      this.getPages()
+    },
+    data() {
+      return {
+        activePage: 0,
+        pages: [],
+      }
+    },
+    methods: {
+      async getPages() {
+        let res = await fetch("pages.json")
+        let data = await res.json()
 
-                this.pages = data;
-            }
-        }
-    }
+        this.pages = data
+      },
+      pageCreated(pageObj) {
+        console.log(pageObj)
+      },
+    },
+  }
 </script>
