49caaf663d442892c25faa34d1627578681162ee
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-20 08:58:24 +0200

    add router params

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -2,21 +2,14 @@
   <div class="container">
     <h1 class="emphesize">Page Title</h1>
     <p>Page Content</p>
+    <p>{{ $route.params.index }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    props: {
-      page: {
-        type: Object,
-        default(rawProps) {
-          return {
-            pageTitle: "",
-            content: "",
-          }
-        },
-      },
+    created() {
+      //   console.log(this.$route.params)
     },
   }
 </script>
